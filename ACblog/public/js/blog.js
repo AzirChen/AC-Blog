46dfd0e483489861c485dd9a998b6f8539275a35
AzirChen <958173292@qq.com>
2016-08-17 17:43:41 +0800

    new

---
+++ ACblog/public/js/blog.js
@@ -0,0 +1,20 @@
+
+/*$('#btn-login').click(function(){
+	      alert("to login");
+				var username = $('#username').val();
+				var password = $('#password').val();
+        if (username.length()<2&&username.length()>8) {
+        	alert("有户名长度在2~8之间");
+        	$("username").css("border","1px solid red");
+        	return false;
+        }
+			});
+$('#btn-reg').click(function(){
+				var username = $('#username').val();
+				var password = $('#password').val();
+        if (username.length()<2||username.length()>8) {
+        	alert("有户名长度在2~8之间");
+        	$("username").css("border","1px solid red");
+        	return;
+        }
+			});*/
